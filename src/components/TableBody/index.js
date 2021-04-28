aa9ca80b9950a18430c9fdcae4a0d319d4376fe6
MattV <mvpetersen1@gmail.com>
2021-04-27 21:07:15 -0600

    fixed tablebodyjs to line up

--- src/components/TableBody/index.js
+++ src/components/TableBody/index.js
@@ -2,12 +2,26 @@ import React from 'react'
 
 export default function index(image, fname, lname, email, phone, state) {
   return (
-    <div>
+    
       <tr>
-
+      <td>
+        Image
+          {/* <div className="img-container">
+            <img alt={fname} src={image} />
+          </div> */}
+        </td>
+        <td>
+          Matthew Petersen
+        </td>
+        <td>
+          bob@bob.com
+        </td>
+        <td>
+          Utah
+        </td>
 
       </tr>
-    </div>
+    
   )
 }
 
